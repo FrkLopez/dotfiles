0099911f076c4ceaa93301f46c52b31332c20d0a
FrkLopez <franklopezolivares@gmail.com>
2018-04-29 16:50:34 +0200

    Deleted files

--- .emacs.d/elpa/popup-20160709.729/popup-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("popup.el") (23235 25446 631274 155000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; popup-autoloads.el ends here
